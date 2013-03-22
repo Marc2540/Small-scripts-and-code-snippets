7cc7244ccba5741a0014399597b6a36840576548
Marcus <MWQ@live.dk>
2013-03-22 09:44:27 +0100

    Update Pig Latin Translator.py

--- Pig Latin Translator.py
+++ Pig Latin Translator.py
@@ -8,7 +8,7 @@ if len(original) > 0 and original.isalpha():
   #print original  #debugging
   word = original.lower()
   first = word[0]
-  if word[0] == 'a' or word[0] == 'e' or word[0] == 'i' or word[0] == 'o' or word[0] == 'u':
+  if word[0] in ['a','e','i','o','u']:
     #print ('vowel')  #debugging
     new_word=original + pyg
     print (new_word)
